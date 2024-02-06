813bd4e56d65856b686f3b78387be432b3c2f338
WirreXx <wisam@5thplanetgames.com>
2024-02-06 14:40:33 +0100

    Updating Git function

--- lite_build.py
+++ lite_build.py
@@ -1,4 +1,10 @@
 # here goes the functions for the sql, Please Use virtual env (venv) when working on this code
 import sqlite3
 
+# W: create connection
+
+# W: create table
+
+# W: add_products
+
 
